69fe0c64bb89b679f0b4a08529814e196dfbc868
AbiyuTolosa <abiyunigussie7@gmail.com>
2022-02-08 13:29:12 -0800

    image src added

--- scrap.py
+++ scrap.py
@@ -6,11 +6,12 @@ def news():
     page = requests.get(
         "https://soccerethiopia.net/football/category/news").text
     soup = BeautifulSoup(page, 'lxml')
-    datas = soup.find_all('div', class_='entry-grid-content')
+    datas = soup.find_all('div', class_='entry-grid hgrid')
     allData = []
     id = 0
     for data in datas:
         topicarr = []
+        image = data.find('img', itemprop='image').get('src')
         headline = data.find('a', itemprop='url').text
         author = data.find('span', itemprop='name').text
         dataPublished = data.find('time', itemprop='datePublished').text
@@ -22,6 +23,7 @@ def news():
         topic = ", ". join(topicarr)
         allData.append({
             "id": id,
+            "image": image,
             "headline": headline,
             "author": author,
             "datePublished": dataPublished,
